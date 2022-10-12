f3ac333207d9373ea6f48c6dda9ecda9a6ec37db
James Mohan <c20305011@mytudublin.ie>
2022-10-12 15:01:32 +0100

    Tea Making!!!!

--- Tea&D/Assets/Scripts/Tea.cs
+++ Tea&D/Assets/Scripts/Tea.cs
@@ -4,32 +4,57 @@ using UnityEngine;
 
 public class Tea : MonoBehaviour
 {
-    public Animator animator;
-    private bool teaBag = false;
-    private bool milk = false;
+    public string nameOfTea;
 
-    public void OnTriggerEnter2D(Collider2D other)
+    [Header("Tea Bag")]
+    public string acceptedTeaBag;
+    public bool teaBag;
+
+    [Header("Milk")]
+    public string acceptedMilk;
+    public bool milk;
+
+    [Header("Sugar")]
+    public string acceptedSugar;
+    public bool sugar;
+
+
+    public void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.tag == "TeaBag")
+
+        if (other.gameObject.tag == acceptedTeaBag)
         {
-            Destroy(other.gameObject);
             teaBag = true;
-        }
-
-        if (other.gameObject.tag == "Milk")
-        {
             Destroy(other.gameObject);
-            milk = true;
         }
-    }
 
-    private void Update()
-    {
-        if (teaBag && milk)
+        if (other.gameObject.tag == acceptedMilk)
         {
-            animator.SetBool("TeaMade", true);
+            milk = true;
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == acceptedSugar)
+        {
+            sugar = true;
+            Destroy(other.gameObject);
         }
     }
 
-    
+    // Update is called once per frame
+    void Update()
+    {
+        if (teaBag && milk && sugar)
+        {
+            this.gameObject.tag = nameOfTea;
+        }
+    }
+
+    /*
+     * A string that records the tag for each ingredient type used in the recepie
+     * 
+     * If the tag is true destroy the other object and set a variable for each ingredient type to true
+     * 
+     * If they are all true change the tag of this gameobject to the corresponding tag to the customer
+     */
 }
