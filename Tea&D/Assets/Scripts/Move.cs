8e83dddc54def942ffcf8ca0fafd74b0b25622a7
Tox1cQueen <77941882+Tox1cQueen@users.noreply.github.com>
2022-10-15 18:05:41 +0100

    Customer Leaving :)

--- Tea&D/Assets/Scripts/Move.cs
+++ Tea&D/Assets/Scripts/Move.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] Transform[] Positions;
+    [SerializeField] float Speed;
+    int NextPosIndex;
+    Transform NextPos;
     public bool Canleave = false;
 
     // Start is called before the first frame update
-    void Awake()
-    {
-        
-    }
-
     void Start()
     {
-
+        NextPos = Positions[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveGameObject();
+    }
+
+    void MoveGameObject()
+    {
+          if (transform.position == NextPos.position)
+          {
+            if (Canleave == true)
+            {
+                NextPosIndex++;
+                if (NextPosIndex >= Positions.Length)
+                {
+                    NextPosIndex = 0;
+                }
+                NextPos = Positions[NextPosIndex];
+            }
+          }
+          else
+          {
+                transform.position = Vector3.MoveTowards(transform.position, NextPos.position, Speed * Time.deltaTime);
+          }
     }
 }
